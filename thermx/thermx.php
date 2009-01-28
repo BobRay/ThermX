5db49b29c9394ed7e3f3045d8e41e8cf64dd6f10
BobRay <BobRay@24346d65-fb89-4855-a768-f5fb827ddd76>
2009-01-28 05:01:15 +0000

    Updated version numbers for Revo.
    git-svn-id: http://svn.modxcms.com/svn/modx-components/thermx/trunk@395 24346d65-fb89-4855-a768-f5fb827ddd76

--- thermx/thermx.php
+++ thermx/thermx.php
@@ -3,7 +3,7 @@
  @package thermx
  @author: Bob Ray
  @created: 01/15/2009
- @version 1.0.0
+ @version 3.0.1
  Adapted from a Wordpress Plugin by Christopher Ross
  Original Plugin URI: http://thisismyurl.com
  Compatibility: MODx Revolution
